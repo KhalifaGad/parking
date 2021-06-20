2df361d391b1b29078b720e84d208c00169dcaaa
KhalifaGad <kkhalifa.gad@gmail.com>
2021-06-20 09:00:23 +0200

    init project structure and init http server

---
+++ src/interfaces/http/server.js
@@ -0,0 +1,40 @@
+import express from "express";
+import cors from "cors";
+
+class Server {
+  constructor(port) {
+    this.port = port;
+    this.app = express();
+    this.setup();
+  }
+
+  setup() {
+    const whitelist = ["http://localhost:3000"];
+    this.app
+      .use(express.urlencoded({ extended: true }))
+      .use(express.json())
+      .disable("x-powered-by")
+      .use(
+        cors((req, callback) => {
+          let options = {
+            credentials: true,
+            origin: false,
+          };
+          const origin = req.header("Origin");
+          const xOrigin = req.header("x-origin");
+
+          if (whitelist.indexOf(origin) !== -1) options.origin = true;
+          else if (origin === null && xOrigin === "mobile")
+            options.origin = true;
+
+          return callback(null, options);
+        })
+      );
+
+    this.app.use((req, res, _next) => {
+      if (!req.route) res.status(404).send("Not found!");
+    });
+  }
+}
+
+export default Server;
